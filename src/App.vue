ae648b100d113a0e5fc960765808d3ca69eab542
Baiyibing <baiyibing@gmail.com>
2017-02-23 21:01:21 +0800

    wu

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,96 @@
 <template>
-	<div id="app">
-		<transition name="bounce">
-			<router-view></router-view>
-		</transition>
-	</div>
+    <div id="app">
+        <!--我们可以用 <transition> 组件给它添加一些过渡效果-->
+        <transition name="bounce">
+            <!-- 路由出口 -->
+            <!-- 路由匹配到的组件将渲染在这里 -->
+            <!-- 如果两个结合一起用，要确保在内层使用 -->
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
+        </transition>
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  components: {
-  },
-  created:function(){
-	  this.$router.replace('/login')
-  }
-}
+    export default {
+        name: 'app',
+        components: {},
+        created: function () {
+            // 组件创建完后执行
+            this.$router.replace('/login')
+            //除了使用 <router-link> 创建 a 标签来定义导航链接，我们还可以借助 router 的实例方法，通过编写代码来实现。
+            //想要导航到不同的 URL，则使用 router.push 方法。这个方法会向 history 栈添加一个新的记录，所以，当用户点击浏览器后退按钮时，则回到之前的 URL。
+            //当你点击 <router-link> 时，这个方法会在内部调用，所以说，点击 <router-link :to="..."> 等同于调用 router.push(...)。
+            //跟 router.push 很像，唯一的不同就是，它不会向 history 添加新记录，而是跟它的方法名一样 —— 替换掉当前的 history 记录。
+            //router.go(n)这个方法的参数是一个整数，意思是在 history 记录中向前或者后退多少步，类似 window.history.go(n)
+        }
+    }
 </script>
 
 <style>
-	@import "../node_modules/font-awesome/css/font-awesome.min.css";
-	@import "../node_modules/flex.css/dist/flex.css";
-	.bounce-enter-active {
-		animation: bounce-in .5s;
-	}
-	
-	.bounce-leave-active {
-		animation: bounce-out .2s;
-	}
-	
-	@keyframes bounce-in {
-		0% {
-			transform: scale(0);
-		}
-		50% {
-			transform: scale(1.05);
-		}
-		100% {
-			transform: scale(1);
-		}
-	}
-	
-	@keyframes bounce-out {
-		0% {
-			transform: scale(1);
-		}
-		50% {
-			transform: scale(0.95);
-		}
-		100% {
-			transform: scale(0);
-		}
-	}
-	
-	body {
-		/*background-color: #324057;*/
-		margin: 0px;
-		padding: 0px;
-		/*background: url(assets/bg1.jpg) center !important;
-		background-size: cover;*/
-		background: #1F2D3D;
-		font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-		font-weight: 400;
-		-webkit-font-smoothing: antialiased;
-	}
-	
-	#app {
-		position: absolute;
-		top: 0px;
-		bottom: 0px;
-		width: 100%;
-	}
-	
-	.el-submenu [class^=fa] {
-		vertical-align: baseline;
-		margin-right: 10px;
-	}
-	
-	.el-menu-item [class^=fa] {
-		vertical-align: baseline;
-		margin-right: 10px;
-	}
+    @import "../node_modules/font-awesome/css/font-awesome.min.css";
+    @import "../node_modules/flex.css/dist/flex.css";
+
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+
+    .bounce-leave-active {
+        animation: bounce-out .2s;
+    }
+
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.05);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+
+    @keyframes bounce-out {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(0.95);
+        }
+        100% {
+            transform: scale(0);
+        }
+    }
+
+    body {
+        /*background-color: #324057;*/
+        margin: 0px;
+        padding: 0px;
+        /*background: url(assets/bg1.jpg) center !important;
+        background-size: cover;*/
+        background: #1F2D3D;
+        font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    #app {
+        position: absolute;
+        top: 0px;
+        bottom: 0px;
+        width: 100%;
+    }
+
+    .el-submenu [class^=fa] {
+        vertical-align: baseline;
+        margin-right: 10px;
+    }
+
+    .el-menu-item [class^=fa] {
+        vertical-align: baseline;
+        margin-right: 10px;
+    }
 </style>
